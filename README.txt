fd7b695c4b2769075159873e7b1dcbb589dcde0e
Pathikrit Bhowmick <pathikrit@addepar.com>
2012-10-22 16:16:03 -0700

    first working thing

---
+++ README.txt
@@ -0,0 +1,16 @@
+Mac OSX Mountain Lion
+0. Install HomeBrew
+1. Install https://xquartz.macosforge.org
+2. brew install opencv
+
+Linux:
+
+
+
+
+Finally, Open IntelliJ -> File -> Open Project -> Open the .iml file
+
+
+
+
+
---
+++ src/CaptureImage.java
@@ -0,0 +1,23 @@
+import com.googlecode.javacv.OpenCVFrameGrabber;
+
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+
+public class CaptureImage {
+  private static void captureFrame() {
+    // 0-default camera, 1 - next...so on
+    final OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+    try {
+      grabber.start();
+      IplImage img = grabber.grab();
+      if (img != null) {
+        cvSaveImage("capture.jpg", img);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  public static void main(String[] args) {
+    captureFrame();
+  }
+}
