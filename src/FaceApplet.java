9e7951fad52989ff5841f6246d8005256c94d5e1
Pathikrit Bhowmick <pathikrit@addepar.com>
2012-10-22 17:49:05 -0700

    2-spaces

--- src/FaceApplet.java
+++ src/FaceApplet.java
@@ -28,111 +28,107 @@ import static com.googlecode.javacv.cpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNIN
 import static com.googlecode.javacv.cpp.opencv_objdetect.CvHaarClassifierCascade;
 import static com.googlecode.javacv.cpp.opencv_objdetect.cvHaarDetectObjects;
 
-/**
- *
- * @author Samuel Audet
- */
 public class FaceApplet extends Applet implements Runnable {
 
-    private CvHaarClassifierCascade classifier = null;
-    private CvMemStorage storage = null;
-    private FrameGrabber grabber = null;
-    private IplImage grabbedImage = null, grayImage = null, smallImage = null;
-    private CvSeq faces = null;
-    private boolean stop = false;
-    private Exception exception = null;
+  private CvHaarClassifierCascade classifier = null;
+  private CvMemStorage storage = null;
+  private FrameGrabber grabber = null;
+  private IplImage grabbedImage = null, grayImage = null, smallImage = null;
+  private CvSeq faces = null;
+  private boolean stop = false;
+  private Exception exception = null;
 
-    @Override public void init() {
-        try {
-            File classifierFile = Loader.extractResource("haarcascade_frontalface_alt.xml", null, "classifier", ".xml");
-            Loader.load(opencv_objdetect.class); // Preload the opencv_objdetect module to work around a known bug.
-            classifier = new CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
-            classifierFile.delete();
-            if (classifier.isNull()) {
-                throw new IOException("Could not load the classifier file.");
-            }
-            storage = CvMemStorage.create();
-        } catch (Exception e) {
-            e.printStackTrace();
+  @Override public void init() {
+    try {
+      File classifierFile = Loader.extractResource("haarcascade_frontalface_alt.xml", null, "classifier", ".xml");
+      Loader.load(opencv_objdetect.class); // Preload the opencv_objdetect module to work around a known bug.
+      classifier = new CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
+      classifierFile.delete();
+      if (classifier.isNull()) {
+        throw new IOException("Could not load the classifier file.");
+      }
+      storage = CvMemStorage.create();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override public void start() {
+    new Thread(this).start();
+  }
+
+  public void run() {
+    try {
+      try {
+        grabber = FrameGrabber.createDefault(0);
+        grabber.setImageWidth(getWidth());
+        grabber.setImageHeight(getHeight());
+        grabber.start();
+        grabbedImage = grabber.grab();
+      } catch (Exception e) {
+        if (grabber != null) grabber.release();
+        grabber = new OpenCVFrameGrabber(0);
+        grabber.setImageWidth(getWidth());
+        grabber.setImageHeight(getHeight());
+        grabber.start();
+        grabbedImage = grabber.grab();
+      }
+      grayImage  = IplImage.create(grabbedImage.width(),   grabbedImage.height(),   IPL_DEPTH_8U, 1);
+      smallImage = IplImage.create(grabbedImage.width()/4, grabbedImage.height()/4, IPL_DEPTH_8U, 1);
+      stop = false;
+      while (!stop && (grabbedImage = grabber.grab()) != null) {
+        if (faces == null) {
+          cvClearMemStorage(storage);
+          cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
+          cvResize(grayImage, smallImage, CV_INTER_AREA);
+          faces = cvHaarDetectObjects(smallImage, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+          repaint();
         }
+      }
+      grabbedImage = grayImage = smallImage = null;
+      grabber.stop();
+      grabber.release();
+      grabber = null;
+    } catch (Exception e) {
+      if (exception == null) {
+        exception = e;
+        repaint();
+      }
     }
+  }
 
-    @Override public void start() {
-      new Thread(this).start();
-    }
+  @Override public void update(Graphics g) {
+    paint(g);
+  }
 
-    public void run() {
-        try {
-            try {
-                grabber = FrameGrabber.createDefault(0);
-                grabber.setImageWidth(getWidth());
-                grabber.setImageHeight(getHeight());
-                grabber.start();
-                grabbedImage = grabber.grab();
-            } catch (Exception e) {
-                if (grabber != null) grabber.release();
-                grabber = new OpenCVFrameGrabber(0);
-                grabber.setImageWidth(getWidth());
-                grabber.setImageHeight(getHeight());
-                grabber.start();
-                grabbedImage = grabber.grab();
-            }
-            grayImage  = IplImage.create(grabbedImage.width(),   grabbedImage.height(),   IPL_DEPTH_8U, 1);
-            smallImage = IplImage.create(grabbedImage.width()/4, grabbedImage.height()/4, IPL_DEPTH_8U, 1);
-            stop = false;
-            while (!stop && (grabbedImage = grabber.grab()) != null) {
-                if (faces == null) {
-                    cvClearMemStorage(storage);
-                    cvCvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
-                    cvResize(grayImage, smallImage, CV_INTER_AREA);
-                    faces = cvHaarDetectObjects(smallImage, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
-                    repaint();
-                }
-            }
-            grabbedImage = grayImage = smallImage = null;
-            grabber.stop();
-            grabber.release();
-            grabber = null;
-        } catch (Exception e) {
-            if (exception == null) {
-                exception = e;
-                repaint();
-            }
+  @Override public void paint(Graphics g) {
+    if (grabbedImage != null) {
+      BufferedImage image = grabbedImage.getBufferedImage(2.2/grabber.getGamma());
+      Graphics2D g2 = image.createGraphics();
+      if (faces != null) {
+        g2.setColor(Color.RED);
+        g2.setStroke(new BasicStroke(2));
+        int total = faces.total();
+        for (int i = 0; i < total; i++) {
+          CvRect r = new CvRect(cvGetSeqElem(faces, i));
+          g2.drawRect(r.x()*4, r.y()*4, r.width()*4, r.height()*4);
         }
+        faces = null;
+      }
+      g.drawImage(image, 0, 0, null);
     }
-
-    @Override public void update(Graphics g) {
-        paint(g);
+    if (exception != null) {
+      int y = 0, h = g.getFontMetrics().getHeight();
+      g.drawString(exception.toString(), 5, y += h);
+      for (StackTraceElement e : exception.getStackTrace()) {
+        g.drawString("    at " + e.toString(), 5, y += h);
+      }
     }
+  }
 
-    @Override public void paint(Graphics g) {
-        if (grabbedImage != null) {
-            BufferedImage image = grabbedImage.getBufferedImage(2.2/grabber.getGamma());
-            Graphics2D g2 = image.createGraphics();
-            if (faces != null) {
-                g2.setColor(Color.RED);
-                g2.setStroke(new BasicStroke(2));
-                int total = faces.total();
-                for (int i = 0; i < total; i++) {
-                    CvRect r = new CvRect(cvGetSeqElem(faces, i));
-                    g2.drawRect(r.x()*4, r.y()*4, r.width()*4, r.height()*4);
-                }
-                faces = null;
-            }
-            g.drawImage(image, 0, 0, null);
-        }
-        if (exception != null) {
-            int y = 0, h = g.getFontMetrics().getHeight();
-            g.drawString(exception.toString(), 5, y += h);
-            for (StackTraceElement e : exception.getStackTrace()) {
-                g.drawString("        at " + e.toString(), 5, y += h);
-            }
-        }
-    }
+  @Override public void stop() {
+    stop = true;
+  }
 
-    @Override public void stop() {
-        stop = true;
-    }
-
-    @Override public void destroy() { }
+  @Override public void destroy() { }
 }
--- src/MotionDetector.java
+++ src/MotionDetector.java
@@ -1,15 +1,3 @@
-/* 
- * I developed some code for recognize motion detections with JavaCV.
- * Actually, it works with an array of Rect, performing, every cicle, an
- * intersection test with area of difference with the rect of interests
- * (this array is callad "sa", stands for SizedArea). I hope could this
- * helps someone.
- * 
- * Feel free to ask about any question regarding the code above, cheers!
- *
- * Angelo Marchesin <marchesin.angelo@gmail.com>
- */
-
 import com.googlecode.javacpp.Loader;
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.OpenCVFrameGrabber;
@@ -32,46 +20,46 @@ import static com.googlecode.javacv.cpp.opencv_imgproc.cvThreshold;
 
 public class MotionDetector {
    public static void main(String[] args) throws Exception {
-      OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
-      grabber.start();
+    OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+    grabber.start();
 
-      IplImage frame = grabber.grab();
-      IplImage image = null;
-      IplImage prevImage = null;
-      IplImage diff = null;
+    IplImage frame = grabber.grab();
+    IplImage image = null;
+    IplImage prevImage = null;
+    IplImage diff = null;
 
-      CanvasFrame canvasFrame = new CanvasFrame("Some Title");
-      canvasFrame.setCanvasSize(frame.width(), frame.height());
+    CanvasFrame canvasFrame = new CanvasFrame("Some Title");
+    canvasFrame.setCanvasSize(frame.width(), frame.height());
 
-      CvMemStorage storage = CvMemStorage.create();
+    CvMemStorage storage = CvMemStorage.create();
 
-      while (canvasFrame.isVisible() && (frame = grabber.grab()) != null) {
-        cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
-        if (image == null) {
-           image = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
-           cvCvtColor(frame, image, CV_RGB2GRAY);
-        } else {
-           prevImage = image;
-           image = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
-           cvCvtColor(frame, image, CV_RGB2GRAY);
-        }
+    while (canvasFrame.isVisible() && (frame = grabber.grab()) != null) {
+    cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
+    if (image == null) {
+       image = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
+       cvCvtColor(frame, image, CV_RGB2GRAY);
+    } else {
+       prevImage = image;
+       image = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
+       cvCvtColor(frame, image, CV_RGB2GRAY);
+    }
 
-        if (diff == null) {
-           diff = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
-        }
+    if (diff == null) {
+       diff = IplImage.create(frame.width(), frame.height(), IPL_DEPTH_8U, 1);
+    }
 
-        if (prevImage != null) {
-           cvAbsDiff(image, prevImage, diff);
-           cvThreshold(diff, diff, 64, 255, CV_THRESH_BINARY);
-           canvasFrame.showImage(diff);
-           CvSeq contour = new CvSeq(null);
-           cvFindContours(diff, storage, contour, Loader.sizeof(CvContour.class), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-           while (contour != null && !contour.isNull()) {
-              contour = contour.h_next();
-           }
-        }
-      }
-      grabber.stop();
-      canvasFrame.dispose();
+    if (prevImage != null) {
+       cvAbsDiff(image, prevImage, diff);
+       cvThreshold(diff, diff, 64, 255, CV_THRESH_BINARY);
+       canvasFrame.showImage(diff);
+       CvSeq contour = new CvSeq(null);
+       cvFindContours(diff, storage, contour, Loader.sizeof(CvContour.class), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+       while (contour != null && !contour.isNull()) {
+        contour = contour.h_next();
+       }
+    }
+    }
+    grabber.stop();
+    canvasFrame.dispose();
    }
 }
