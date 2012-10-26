b3814691dfc6854ac91b95436086f1c174fce0af
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2012-10-26 02:18:28 -0700

    show preview in confirm only

--- src/example/FaceApplet.java
+++ src/example/FaceApplet.java
@@ -1,22 +1,16 @@
 package example;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-import java.util.List;
-
-import javax.swing.JApplet;
-import javax.swing.JOptionPane;
-
 import lib.FaceDb;
 import lib.FacialRecognition;
 import lib.FacialRecognition.PotentialFace;
 import lib.WebCam;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.util.List;
 
 public class FaceApplet extends JApplet implements Runnable, MouseListener {
 
@@ -27,13 +21,15 @@ public class FaceApplet extends JApplet implements Runnable, MouseListener {
   private boolean running = true;
   private boolean dialog = false;
 
-  @Override public void init() {
+  @Override
+  public void init() {
     cam.start();
     setSize(1600, 900);
     addMouseListener(this);
   }
 
-  @Override public void start() {
+  @Override
+  public void start() {
     new Thread(this).start();
   }
 
@@ -43,34 +39,28 @@ public class FaceApplet extends JApplet implements Runnable, MouseListener {
     }
   }
 
-  @Override public void update(Graphics g) {
+  @Override
+  public void update(Graphics g) {
     paint(g);
   }
 
-  @Override public void paint(Graphics g) {
+  @Override
+  public void paint(Graphics g) {
     final BufferedImage image = cam.capture();
     if (image == null || dialog) {
       return;
     }
-    drawFaces(g, image);
+    drawFaces(image);
     g.drawImage(image, 0, 0, null);
   }
 
-  public void drawFaces(Graphics g, BufferedImage image) {
+  public void drawFaces(BufferedImage image) {
     final List<PotentialFace> faces = FacialRecognition.run(image, db);
     if (faces.isEmpty()) {
       return;
     }
     Graphics2D g2 = image.createGraphics();
     g2.setStroke(new BasicStroke(2));
-    if (faces.size() == 1) {
-      Rectangle r = faces.get(0).box;
-      if (currentFace != null) {
-        g.clearRect(image.getWidth(), 0, currentFace.getWidth(), currentFace.getHeight());
-      }
-      currentFace = image.getSubimage(r.x, r.y, r.width, r.height);
-      g.drawImage(currentFace, image.getWidth(), 0, null);
-    }
     for (PotentialFace face : faces) {
       final Rectangle r = face.box;
       final Color c;
@@ -84,15 +74,17 @@ public class FaceApplet extends JApplet implements Runnable, MouseListener {
       }
       g2.setColor(c);
       g2.drawRect(r.x, r.y, r.width, r.height);
-      g2.drawString(msg, r.x+5, r.y-5);
+      g2.drawString(msg, r.x + 5, r.y - 5);
     }
   }
 
-  @Override public void stop() {
+  @Override
+  public void stop() {
     running = false;
   }
 
-  @Override public void destroy() {
+  @Override
+  public void destroy() {
     cam.stop();
   }
 
@@ -105,7 +97,7 @@ public class FaceApplet extends JApplet implements Runnable, MouseListener {
       return;
     }
     dialog = true;
-    final String name = JOptionPane.showInputDialog(null, "Save as?");
+    final String name = (String) JOptionPane.showInputDialog(null, "Save as?", "Tag", JOptionPane.QUESTION_MESSAGE, new ImageIcon(currentFace, "Preview"), null, null);
     dialog = false;
     if (name != null) {
       System.out.println("Saving " + name + "...");
@@ -114,14 +106,18 @@ public class FaceApplet extends JApplet implements Runnable, MouseListener {
   }
 
   @Override
-  public void mousePressed(MouseEvent mouseEvent) {}
+  public void mousePressed(MouseEvent mouseEvent) {
+  }
 
   @Override
-  public void mouseReleased(MouseEvent mouseEvent) {}
+  public void mouseReleased(MouseEvent mouseEvent) {
+  }
 
   @Override
-  public void mouseEntered(MouseEvent mouseEvent) {}
+  public void mouseEntered(MouseEvent mouseEvent) {
+  }
 
   @Override
-  public void mouseExited(MouseEvent mouseEvent) {}
+  public void mouseExited(MouseEvent mouseEvent) {
+  }
 }
